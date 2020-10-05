ac10f85d00cce447bb766e430dfac6323c34ca9b
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-05 14:05:01 -0300

    wip

---
+++ resources/[Mapas]/industrial/fxmanifest.lua
@@ -0,0 +1,8 @@
+
+fx_version 'bodacious'
+game 'gta5'
+
+file 'int_industrial.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'int_industrial.ytyp'
+
+--MADE BY MASSAS--
---
+++ resources/[Mapas]/vrp_mapas/__resource.lua
@@ -0,0 +1,22 @@
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map "yes"
+
+client_scripts {
+    "client.lua"
+}
+
+files {
+	"interiorproxies.meta"
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+data_file 'DLC_ITYP_REQUEST' 'stream/DisneyHackerspace/hackerspace.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/DisneyModernhouse/lafa2k_modernhouse.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/DisneyYakuza/yakuza.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vesp_props.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/DisneyIlha/def_props.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/DisneyPraca/lafa2k_halloween.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/DisneyPraca/omegatower.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/DisneyFinalfantasyxv/ffxv_galdinquay.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/DisneyFightClub/fightclub.ytyp'
