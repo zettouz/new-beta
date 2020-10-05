7fb56421b7c4b46ea870b5e9234c0d82497b162a
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-05 16:41:01 -0300

    wip

---
+++ resources/[Veículos]/cp_xj6/data/tuningxj6.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+	AddTextEntryByHash(GetHashKey("xj6_cano_a"),"Tuning 01")
+
+end)
---
+++ resources/[Veículos]/cp_xj6/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+	"data/handling.meta",
+	"data/vehicles.meta",
+	"data/carcols.meta",
+	"data/carvariations.meta"
+}
+
+client_script "data/tuningxj6.lua"
+
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
