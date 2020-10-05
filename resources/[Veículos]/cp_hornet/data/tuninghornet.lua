d5bde5dd48a4b8632cd1c5be2f283c7e335d2415
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-05 16:38:24 -0300

    wip

---
+++ resources/[Veículos]/cp_hornet/data/tuninghornet.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+	AddTextEntryByHash(GetHashKey("hornet_cano_a"),"Tuning 01")
+	AddTextEntryByHash(GetHashKey("hornet_escape_a"),"Tuning 02")
+	AddTextEntryByHash(GetHashKey("hornet_suporte_a"),"Tuning 03")
+	AddTextEntryByHash(GetHashKey("hornet_suporte2_a"),"Tuning 04")
+
+end)
