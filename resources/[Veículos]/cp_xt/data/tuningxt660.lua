125269ce19bed0dddb330ad70b61fabbbfc02af0
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-05 16:41:25 -0300

    wip

---
+++ resources/[Veículos]/cp_xt/data/tuningxt660.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+	AddTextEntryByHash(GetHashKey("xt660_cano_a"),"Tuning 01")
+	AddTextEntryByHash(GetHashKey("xt660_churra_a"),"Tuning 02")
+	AddTextEntryByHash(GetHashKey("xt660_escape_b"),"Tuning 03")
+	AddTextEntryByHash(GetHashKey("xt660_ferros_a"),"Tuning 04")
+	AddTextEntryByHash(GetHashKey("xt660_ferros_a"),"Tuning 05")
+
+end)
