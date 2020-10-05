b91e206155edef48b84c9ca8493614669254e0e0
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-05 13:58:30 -0300

    wip

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo       Jackson
+echo ------------------------
+echo   Jackson Silva#9911
+echo ------------------------
+
+
+pause
+start ..\build\FXServer.exe +exec config.cfg +set onesync_enableInfinity 1
+exit
