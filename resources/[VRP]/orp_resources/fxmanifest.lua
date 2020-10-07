d43bc827f1ccb1f9266c6623e5f47ba2248badc2
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-07 04:44:56 -0700

    wip

--- resources/[VRP]/orp_resources/fxmanifest.lua
+++ resources/[VRP]/orp_resources/fxmanifest.lua
@@ -4,13 +4,11 @@ game 'gta5'
 client_script { 
 	"@vrp/lib/utils.lua",
 	--"tacklec.lua",
-	"radarC.lua"
-	--"idc.lua"
+	"idc.lua"
 }
 
 server_script {
 	"@vrp/lib/utils.lua",
 	--"tackles.lua",
-	"radarS.lua"
-	--"ids.lua"
+	"ids.lua"
 }
