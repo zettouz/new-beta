6248d5143f57b1a09a8bf181f0fc92dabc0108d5
Jackson Silva <jacksonsilvadev@gmail.com>
2020-10-08 00:53:02 -0300

    wip

---
+++ resources/[VRP]/vrp_ktackle/server/main.lua
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VRP
+-----------------------------------------------------------------------------------------------------------------------------------------
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CONEXÃO
+-----------------------------------------------------------------------------------------------------------------------------------------
+src = {}
+Tunnel.bindInterface("vrp_ktable",src)
+vCLIENT = Tunnel.getInterface("vrp_ktable")
+
+RegisterServerEvent('jackson:tryTackle')
+AddEventHandler('jackson:tryTackle', function()
+	local source = source
+	local user_id = vRP.getUserId(source)
+	local nplayer = vRPclient.getNearestPlayer(source,3)
+
+	local targetPlayer = GetPlayerPed(nplayer)
+
+	TriggerClientEvent('jackson:getTackled', targetPlayer, source)
+	TriggerClientEvent('jackson:playTackle', source)
+end)
